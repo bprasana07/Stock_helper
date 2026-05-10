14a3168d04a40c40d1142e9d3e7a328eee7c43dd
Prasana <bprasana85@gmail.com>
2026-05-10 13:09:47 +0100

    Update stock_screener_app_v1.1.py

--- stock_screener_app_v1.1.py
+++ stock_screener_app_v1.1.py
@@ -901,4 +901,4 @@ if __name__ == "__main__":
     print("\n  GLOBAL STOCK SCREENER -- Starting...")
     print("  Open browser at:  http://localhost:5000")
     print("  Type any company name or ticker symbol\n")
-    app.run(debug=True, port=5000)
+   app.run(debug=True, host="0.0.0.0", port=5000)
